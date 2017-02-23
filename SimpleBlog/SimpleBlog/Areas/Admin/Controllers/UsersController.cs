79d930534f5b67bdf7150e847bd503e47caaecfc
PC20830\L20805 <batuhancabukkazan@gmail.com>
2017-02-23 17:53:58 +0200

    First Commit

---
+++ SimpleBlog/SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            return Content("Admin Area Users Controller Index Action");
+        }
+        public ActionResult List()
+        {
+            return Content("Admin Area Users  Controller List Action");
+        }
+        public ActionResult Edit(int id=0)
+        {
+            return Content("Admin Area Users  Controller Edit Action" + id.ToString());
+        }
+    }
+}
