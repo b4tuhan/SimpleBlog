7cf44f9e1cc59d43184afd7da369477049877e65
PC20830\L20805 <batuhancabukkazan@gmail.com>
2017-03-02 18:12:05 +0200

    First Commit

--- SimpleBlog/SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/SimpleBlog/Controllers/AuthController.cs
@@ -11,7 +11,7 @@ namespace SimpleBlog.Controllers
         // GET: Auth
         public ActionResult Login()
         {
-            return Content("Auth Controller Index Action");
+            return View();
         }
     }
 }
