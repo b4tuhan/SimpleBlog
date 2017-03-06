9c7ab7a236e3f2aa5eb371166a85c816d783fdaf
PC20830\L20805 <batuhancabukkazan@gmail.com>
2017-03-06 18:27:11 +0200

    First Commit

---
+++ SimpleBlog/SimpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.ViewModels
+{
+    public class Auth
+    {
+
+        public int UserName { get; set; }
+        public int Password { get; set; }
+    }
+}
